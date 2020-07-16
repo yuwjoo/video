23774e2a0037c428e26a0c116636fd4345eced29
yuwjoo <1191759350@qq.com>
2020-07-16 08:38:34 +0800

    frist

---
+++ js/chunk-2d0d3e80.0a7afe32.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d3e80"],{"5f81":function(e,t,n){"use strict";n.r(t);var s=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"details"},[e._v(" details ")])},l=[],a=n("2877"),c={},i=Object(a["a"])(c,s,l,!1,null,null,null);t["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0d3e80.0a7afe32.js.map
